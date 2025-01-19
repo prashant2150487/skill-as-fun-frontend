bf453170c2e11e06aeffe1d8203f561d46224a19
Prashant Sachan <p-prashantsachan@goodearth.in>
2025-01-19 22:45:17 +0530

    fix respoince ness of header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,19 +1,18 @@
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight, Menu } from "lucide-react";
 import { FC } from "react";
-import logo  from "../../assets/logoMobiile.svg";
+import logo from "../../assets/logoMobiile.svg";
+import { Button } from "../ui/button";
 
 const Header: FC = () => {
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-800 shadow-md">
-      <nav className="bg-white py-4 md:py-2 border-gray-200 dark:bg-gray-800">
+      <nav className= "bg-white py-1 md:py-2 border-gray-200 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
           <a href="#" className="flex items-center">
-            <img src={logo} alt="logo" />
-            {/* <span className="self-center text-4xl font-semibold whitespace-nowrap text-black dark:text-white">
-              Skillwizard
-            </span> */}
+            <img src={logo} alt="logo" className="size-14 md:size-20"/>
+            
           </a>
-          <div className="flex items-center gap-x-4 md:gap-x-20">
+          <div className="hidden  md:flex items-center gap-x-4 md:gap-x-14">
             <a href="/about" className="text-black text-xl">
               About
             </a>
@@ -25,6 +24,9 @@ const Header: FC = () => {
               <ArrowUpRight />
             </button>
           </div>
+          <Button size="icon" variant="outline" className="md:hidden">
+            <Menu size={20}/>
+          </Button>
         </div>
       </nav>
     </header>
