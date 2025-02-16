c429078e508331d0faab46ce4da043ce3c6b024a
Your Name <your-email@example.com>
2025-02-16 09:36:44 +0530

    fixed logo issue

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import { ArrowUpRight, Menu } from "lucide-react";
 import { FC } from "react";
 import logo from "../../assets/logoMobiile.svg";
+import desktopLogo from "../../assets/logoDesktop.png"
 import { Button } from "../ui/button";
 import {
   DropdownMenu,
@@ -16,13 +17,14 @@ const Header: FC = () => {
       <nav className="bg-white py-1 md:py-2 border-gray-200 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
           <a href="/" className="flex items-center">
-            <img src={logo} alt="logo" className="size-14 md:size-20" />
+            <img src={desktopLogo} alt="logo" className="hidden md:flex" />
+            <img src={logo} alt="logo" className="size-14 md:size-20 md:hidden" />
           </a>
           <div className="hidden  md:flex items-center gap-x-4 md:gap-x-14">
             <a href="/about" className="text-black text-xl pointer-events-none" >
               About
             </a>
-            <a href="/prog  " className="text-black text-xl pointer-events-none">
+            <a href="/prog" className="text-black text-xl pointer-events-none">
               Programs
             </a>
             <button disabled className="bg-[#2E31A6] px-5 py-3 rounded-3xl flex gap-1 shadow-sm text-white ">
