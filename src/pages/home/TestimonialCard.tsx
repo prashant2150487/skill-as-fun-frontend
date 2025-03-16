ffd1f38e5e3c01270dfcc2f298f408cb12e2b136
Prashant Sachan <your-email@example.com>
2025-03-16 07:57:20 +0530

    carousel added

--- src/pages/home/TestimonialCard.tsx
+++ src/pages/home/TestimonialCard.tsx
@@ -2,6 +2,19 @@ import React from "react";
 import img1 from "../../assets/studentProfile/arav.jpeg";
 import img2 from "../../assets/studentProfile/Darsh.jpeg";
 import img3 from "../../assets/studentProfile/sarthak.jpeg";
+import img4 from "../../assets/studentProfile/akshaj.jpeg";
+import img5 from "../../assets/studentProfile/Tanvi.jpeg";
+import img6 from "../../assets/studentProfile/anaisha.jpeg";
+import Autoplay from "embla-carousel-autoplay"
+
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 interface TestimonialCardProps {
   imageSrc: string;
@@ -31,13 +44,37 @@ const testimonials = [
   {
     imageSrc: img3,
     quote:
-      "From zero to coding hero! SkillAsFun helped me understand programming concepts and build real-world applications that amaze my friends.",
+      "SkillAsFun made coding simple and engaging. I love how I can now build my own websites and animations with confidence!",
     studentName: "Sarthak Sachan",
     studentClass: "Class 8",
     studentSchool: "Puranchandra Vidyaniketan, Kanpur",
   },
-  
+  {
+    imageSrc: img4,
+    quote:
+      "I never thought coding could be this exciting! SkillAsFun helped me turn my ideas into working projects, and I enjoy every moment of learning.",
+    studentName: "Akshaj Singh",
+    studentClass: "Class 5",
+    studentSchool: "Father Agnel School, Noida",
+  },
+  {
+    imageSrc: img5,
+    quote:
+      "With SkillAsFun, I discovered my passion for coding. Now, I can build interactive stories and games that my friends love to play!",
+    studentName: "Tanvi Rajput",
+    studentClass: "Class 7",
+    studentSchool: "Apeejay School, Noida",
+  },
+  {
+    imageSrc: img6,
+    quote:
+      "Coding was intimidating at first, but SkillAsFun made it so easy! Now, I can create apps and websites that bring my ideas to life.",
+    studentName: "Anaisha Niranjan",
+    studentClass: "Class 6",
+    studentSchool: "Delhi Public School, Noida",
+  },
 ];
+
 const TestimonialCard: React.FC<TestimonialCardProps> = ({
   imageSrc,
   quote,
@@ -45,24 +82,29 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({
   studentClass,
   studentSchool,
 }) => (
-  <div className="flex flex-col items-center w-[366px] bg-white gap-0 rounded-3xl border border-[#000000] px-4 py-7 box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;">
-    <div className="mb-4 overflow-hidden">
+  <div className="flex flex-col items-center w-full sm:max-w-sm bg-white rounded-3xl border border-gray-200 shadow-lg overflow-hidden">
+    <div className="w-full pt-[75%] relative">
       <img
         src={imageSrc}
         alt={`${studentName}'s portrait`}
-        className=" w-full h-full max-h-72 min-w-44"
+        className="w-full h-full object-cover absolute top-0 inset-0"
       />
     </div>
-    <p className="mb-4 text-lg font-normal text-[#000000] ">
-      &quot;{quote}&quot;
-    </p>
-    <div className="text-left w-full">
-      <h3 className="font-normal text-left max-w-fit">
-        {studentName}
-        {", "}
-        {studentClass}
-      </h3>
-      <p className="text-sm text-gray-600">{studentSchool}</p>
+
+    <div className="p-4 sm:p-5 w-full">
+      <div className="mb-4">
+        <p className="text-sm sm:text-base lg:text-lg italic text-gray-800 font-light leading-relaxed line-clamp-4">
+          "{quote}"
+        </p>
+      </div>
+
+      <div className="border-t pt-4">
+        <h3 className="text-base sm:text-lg font-medium text-gray-900">
+          {studentName}
+          <span className="font-normal text-gray-700">, {studentClass}</span>
+        </h3>
+        <p className="text-xs sm:text-sm truncate text-gray-600">{studentSchool}</p>
+      </div>
     </div>
   </div>
 );
@@ -70,12 +112,29 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({
 export default function TestimonialCards() {
   return (
     <>
-      <div className="container py-8 g-0 mx-auto ">
-        <div className="flex justify-center gap-4  md:gap-8 flex-col items-center md:flex-row">
-          {testimonials.map((testimonial, index) => (
-            <TestimonialCard key={index} {...testimonial} />
-          ))}
-        </div>
+      <div className="mx-auto px-4 sm:px-6 lg:px-16 py-8">
+        <Carousel
+          className="w-full"
+          plugins={[
+            Autoplay({
+              delay: 3000,
+            }),
+          ]}
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+        >
+          <CarouselContent className="ml-0 md:-ml-4">
+            {testimonials.map((testimonial, index) => (
+              <CarouselItem key={index} className="pl-2 md:pl-4 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/3 xl:basis-1/4">
+                <TestimonialCard  {...testimonial} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="hidden lg:flex" />
+          <CarouselNext className="hidden lg:flex" />
+        </Carousel>
       </div>
     </>
   );
